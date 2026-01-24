722b8858a9722114b59d465547f8855b3b4550f6
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-01-24 18:24:06 +0700

    Initilize spring project

---
+++ src/main/java/com/paybridge/loan/PaybridgeLoanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paybridge.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaybridgeLoanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaybridgeLoanServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paybridge/loan/PaybridgeLoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paybridge.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaybridgeLoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
